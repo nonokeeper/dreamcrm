89c27b6675f8c0149dcfe28b64d88e46c2a0aae2
costearnaud <costearnaud@yahoo.fr>
2023-01-31 10:33:02 +0100

    modify and delete Data

--- server/routes/api/dataController.js
+++ server/routes/api/dataController.js
@@ -1,11 +1,11 @@
-const { router, db } = require('./mongoDB');
+const { router, db, mongodb } = require('./mongoDB');
 
 //const collection = 'Customers';
 const collectionMeta = 'meta_collections';
 const DEFAULTSIZE = 25;
 
 // import { authenticateToken } from '../../security/index.ts';
-console.log('dataController called once');
+console.log('dataController called');
 
 // Get Metadata
 router.get('/data/meta', (req,res) => {
@@ -48,6 +48,30 @@ router.get('/data', (req, res) => {
   })
 });
 
-//console.log("API CustomersController loaded");
+// Create an entity record
+router.post('/data/:entity', (req,res) => {
+  console.log('params.entity = ', req.params.entity);
+  if (req.params.entity !== undefined) {
+    db.collection(req.params.entity).insertOne(req.body, (err, result) => {
+      if (err) console.log(`Error Create a ${entity} record : `+err);
+      res.status(201).send(result.insertedId)
+    })
+  } else {
+    console.log(`No entity given!`);
+  }
+})
+
+// Delete an entity record
+router.delete('/data/:entity/:id', (req, res) => {
+  // TODO
+  const collection = req.params.entity;
+  const document = req.params.id;
+  console.log(`Suppression dans ${collection} de l'id : ${document}`);
+
+  db.collection(collection).deleteOne({_id: new mongodb.ObjectId(document)}, (error, result) => {
+    (error)? console.log(error) : console.log('Successfully deleted :' + JSON.stringify(result))
+    res.status(200).send('Record deleted')
+  })
+})
 
 module.exports = router
