2343a84b2c9289edbdb423cfae6474b54da3ce8f
costearnaud <costearnaud@yahoo.fr>
2023-05-28 19:24:39 +0200

    v6

--- server/routes/api/trackingController.js
+++ server/routes/api/trackingController.js
@@ -1,6 +1,6 @@
 const { db } = require('./mongoDB');
 const router = require('./router');
-let collectionEmailLog = process.env.LOG_EMAIL;
+let collectionEmailLog = 'Log_Email';
 
 // Get Open tracking
 router.get('/tracking/:email/:jobId', async (req,res) => {
@@ -10,7 +10,7 @@ router.get('/tracking/:email/:jobId', async (req,res) => {
     console.log('trackingController.js > Email : ', req.params['email']);
     console.log('trackingController.js > Job ID : ', req.params['jobId']);
     console.log('trackingController.js > collectionEmailLog : ', collectionEmailLog);
-    if (!collectionEmailLog) {collectionEmailLog = 'Log_Email'}
+
     const result = await db.collection(collectionEmailLog).updateOne(
         { email: email, jobID: jobID },
         {
